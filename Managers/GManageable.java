b16605b3a062c341246b3b234012c38190d43127
Gking <2665241312@qq.com>
2021-04-04 21:19:06 +0800

    first commit

---
+++ Managers/GManageable.java
@@ -0,0 +1,8 @@
+package GTools.Managers;
+public interface GManageable<Index,Content>
+{
+    public Content get(Index index);
+    public void remove(Index i);
+    public void add(Index i,Content c);
+    public void replace(Index i,Content c);
+}
